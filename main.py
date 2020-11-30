fb1c631d5edf2fe6c061693e810b24c154b09294
Izharulhaq <izharhaq00@gmail.com>
2020-11-30 08:11:51 +0700

    Add input method from file

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 import game_functions as Game
 
-size = int(input())
-n_bombs = int(input())
+input_method = input('1. Manual\n2. From file\nSelect input method: ')
+input_method = int(input_method)
+
+size = 0
+n_bombs = 0
 coord = []
-for i in range(n_bombs):
-    point = input().split()
-    coord.append([int(j) for j in point])
+
+if input_method == 1:
+    size = int(input())
+    n_bombs = int(input())
+
+    for i in range(n_bombs):
+        point = input().split()
+        coord.append([int(j) for j in point])
+elif input_method == 2:
+    filename = input('File name: ')
+    with open(filename, 'r') as f:
+        size = int(f.readline())
+        n_bombs = int(f.readline())
+
+        for i in range(n_bombs):
+            point = f.readline().split()
+            coord.append([int(j) for j in point])
+        f.close()
 
 field = Game.MakeField(coord, size, n_bombs)
 opened = [[False for i in range(size)] for j in range (size)]
